b429da583e07db350558ae6dd837f55fd3d31563
Maf <mafaldafariasantos@gmail.com>
2024-11-07 01:01:18 +0000

    First allocations according to frequency - still needs to be tested

--- processExcel.py
+++ processExcel.py
@@ -3,8 +3,8 @@ import pylightxl as xl
 import calendar
 import datetime
 
-MAX = 8
-week_total=3
+MAX = 5
+week_total = 37
 # Semanal, Bisemanal, Trisemanal
 
 class Task:
@@ -24,77 +24,125 @@ class Week:
 
 def storeTasks():
     i=0
-    task_taskdur = [] 
+    list_tasks_id_per_freq = []      
     # each excel line is a list containing task's description, frequency, duration and place
     for x in db.ws('TarefasPY').rows: 
       if  not str(x[0]) or  not str(x[1])  or  not str(x[0]) or not str(x[3]): # discard incomplete lines and header line     
         continue
-      
-      task = Task(i, x[0], x[1], x[3], x[2])
-      if str(x[1]) in freq: 
-          task_taskdur = dict()
-          task_taskdur = freq[str(x[1])]
-          task_taskdur[str(task.id)] = task
-          
+      id = i
+      desc = x[0]
+      frequency = x[1]
+      duration = x[3]
+      place = x[2] 
+      task = Task(id, desc, frequency, duration, place)
+      tasks[id] = task
+      if str(x[1]) in freq: # if frequency already exists , add task id to frequency's tasks list
+          list_tasks_id_per_freq = freq[frequency] # gets tasks ids list  
+          list_tasks_id_per_freq.append(id)
+          freq[frequency] = list_tasks_id_per_freq # aqui
       else:
-          task_taskdur = {str(task.id): task}
-          freq[str(x[1])]= task_taskdur
-    #Debug: print ('Task #',task.id, len(str(x[0])) ,' name ', task.description, ' duration', task.duration)    
+          list_tasks_id_per_freq = [id]   
+          freq[frequency]= list_tasks_id_per_freq
+    #Debug: print ('Task #',task.id, len(str(x[0])) ,' name ', task.description, ' duration', task.duration)        
       i = i+1
 
-def weekly_Tasks(week_number,frequency, week_tasks_duration):
-    tasks = freq[frequency]
-    for task_id in freq[frequency].keys(): 
+
+def weekly_Tasks(frequency, week_tasks_duration):
+    for task_id in freq[frequency]: 
        week_tasks_duration = week_tasks_duration + tasks[task_id].duration 
+    print('   Total weekly tasks duration is ', week_tasks_duration)
     return week_tasks_duration
 
 def frequency_period(frequency):
     match frequency:
         case 'Quinzenal':
-            return 1
+            return 2
         case 'Mensal':
-            return 3
+            return 4
         case 'Bimestral':
-            return 7
+            return 8
         case 'Trimestral':
-            return 11
+            return 13
         case 'Semestral':
-            return 23
+            return 25
         case 'Anual':
-            return 53
+            return 55
     
-def alocateTasks(curr_week_number): # allocate tasks according to frequency, duration, place in these order 
+def allocateTasks(curr_week_number): # one week's tasks according to frequency, duration, place in these order 
+    
     week_tasks_duration = 0
     week_task_list = [] # current week tasks
+    
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>> week ' ,curr_week_number)
+    for frequency in freq.keys(): # according to tasks frequency 
 
-    for frequency in freq.keys(): #go through all the frequencies
+        if round(week_tasks_duration) == MAX:
+           break
+
         if frequency == 'Semanal': 
-           week_tasks_duration=weekly_Tasks(curr_week_number,frequency, week_tasks_duration)
-           week_task_list = list(freq[frequency].keys())         
+           for task_id in freq[frequency]: 
+               week_tasks_duration = week_tasks_duration + tasks[task_id].duration 
+               #print( '  debug TASK', task_id, ' Semanal  duration is ', week_tasks_duration)
+           week_task_list = freq[frequency].copy()              
+      
         else: 
-            tasks = []
-            tasks = freq[frequency]
-            interval = frequency_period(frequency)
-            if (curr_week_number - interval) in tasksperWeek: # check prior frequency tasks
-              prior_frequency_tasks = tasksperWeek[curr_week_number - interval]
-              
-              for task_id in freq[frequency].keys(): 
-               if  len(prior_frequency_tasks) >0 and task_id in prior_frequency_tasks:  #if task was performed , dont allocate
+            interval = frequency_period(frequency)      
+            # interval defines the interval of prior weeks where the task should be executed
+            # print(' debug - frequency:', frequency,' Interval is  ', interval)
+        
+            for task_id in freq[frequency]: 
+              done = 0
+            #   print(' curr week is', curr_week_number, '> ', interval)
+
+
+              if curr_week_number > interval :  #if task was performed precisely on the interval , MUST be allocate
+                prior_week_number =  curr_week_number - interval                
+                if  task_id in tasksperWeek[prior_week_number]: 
+                  week_task_list.append(task_id)
+                  tasksperWeek[curr_week_number]= list(week_task_list)
+                  curr_task = tasks[task_id]
+                  week_tasks_duration = week_tasks_duration + curr_task.duration
                   continue
-               else:# if task was not performed, allocate concerning to place 
+
+              # #if task was performed previously the interval , MUST be allocate TODO relate to interval th e condition
+              prior_week_number = curr_week_number - 1
+              while  prior_week_number > 0 and curr_week_number - prior_week_number <= interval:  
+                  if task_id in tasksperWeek[prior_week_number]:  
+                        # print('>>>>>>>>>>>>>>>>>>>>DOOOOOONE')
+                        done = 1  
+                        break
+                  prior_week_number = prior_week_number - 1                
+                 
+
+
+# week 5,  interval mensal:4, go
+# week 2 ,  interval mensal:4 , no go
+# week 12 ,  interval mensal:4 , go
+            #   print('NO')
+                 #if task was not performed on the interval, try allocate
+              if done == 0:
+                # if task was not performed,check if can be  allocated 
                   curr_task = tasks[task_id]
                   week_tasks_duration = week_tasks_duration + curr_task.duration 
+
                   if week_tasks_duration  <= MAX:
                      week_task_list.append(task_id)
                      tasksperWeek[curr_week_number]= list(week_task_list)
+                    #  print('  ?task YES', curr_task.id, 'curr_task.duration ', curr_task.duration, 'frequency', frequency,' Week duration : ', week_tasks_duration )
                   else:
-                     week_tasks_duration = week_tasks_duration - curr_task.duration 
-    
+                     week_tasks_duration = week_tasks_duration - curr_task.duration
+                    #  print('  ?task NO', curr_task.id, 'curr_task.duration ', curr_task.duration,' Week duration : ', week_tasks_duration  )
+ 
+                  if round(week_tasks_duration) == MAX:
+                    #  print( "  debug break inside for tasks  in frequency")
+                     break
+                     
     tasksperWeek[curr_week_number]= week_task_list
-     #   print(' >> ',frequency, ' and dura; ',week_tasks_duration)
-    print('<<<<<<<<<<<<<<<<<<<<<<<<< week',curr_week_number, len(tasksperWeek[curr_week_number]), ' tasks' ,'| tasks duration:', round(float(week_tasks_duration)), ' hours') 
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>',  len(tasksperWeek[curr_week_number]), ' tasks' ,'| round tasks duration:', round(float(week_tasks_duration)), ' hours') 
     print(tasksperWeek[curr_week_number])
     #week = Week(week_number, week_tasks_duration, week_task_list)
+
+
 
 def write_to_excel():
     wb = openpyxl.load_workbook('/Users/maf/Desktop/tarefas2.xlsx')
@@ -112,19 +160,28 @@ def write_to_excel():
 # readxl returnsa  pylightxl database that holds all worksheets and its data
 db = xl.readxl(fn='/Users/maf/Desktop/tarefas.xlsx')
 
-freq = dict() 
+freq = dict() # key: task's execution frequency, value: list of task_ids
+tasks = dict()# key:task_id, value: Object Task
+
 tasksperWeek = dict() ##  key: week_number, value: Object Week list of tasks_ids and
 
-storeTasks() 
 
-# get Tasks info - dict{ key: frequency, value: dict{ key: task_id, value: task Object} }
+storeTasks() # fill freq and tasks dictionaries 
+print(  '------------------------- Total tasks number is' , len(tasks))
+
 #    current_week = datetime.date(datetime.date.today().year, datetime.date.today().month, datetime.date.today().day).strftime("%V")
+totalCounter = []
 i=1
 while i <= week_total:
- alocateTasks(i)
- i = i+1
+  allocateTasks(i)
+  
+#   totalCounter.append(tasksperWeek[i]) # count distinct allocated tasks
+  i = i+1
+
+
+print("Total allocated tasks", len( set(totalCounter)))
  
-write_to_excel()
+# write_to_excel()
 ## conditions:
 ## frequency is repected
 #### one week tasks duration <=8 hours  
